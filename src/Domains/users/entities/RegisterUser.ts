50310b59e0ad3f89b829d78f47f98b59dd9f2a0b
AR Project <alvenresta@gmail.com>
2023-08-30 20:23:37 +0700

    change role type

--- src/Domains/users/entities/RegisterUser.ts
+++ src/Domains/users/entities/RegisterUser.ts
@@ -1,52 +1,49 @@
 export interface IRegisterUser {
-  username: string
-  password: string
-  fullname: string
-  role: string
+  username: string;
+  password: string;
+  fullname: string;
+  role?: string;
 }
 
 interface IRegisterUserEntities extends IRegisterUser {
-  _verifyPayload(payload: IRegisterUser): void
-
+  _verifyPayload(payload: IRegisterUser): void;
 }
 
 export default class RegisterUser implements IRegisterUserEntities {
-  username: string
-  password: string
-  fullname: string
-  role: string
+  username: string;
+  password: string;
+  fullname: string;
+  role?: string;
   constructor(payload: IRegisterUser) {
-    this._verifyPayload(payload)
+    this._verifyPayload(payload);
 
-    const { username, password, fullname, role }: IRegisterUser = payload
+    const { username, password, fullname, role }: IRegisterUser = payload;
 
-    this.username = username
-    this.password = password
-    this.fullname = fullname
-    this.role = role
+    this.username = username;
+    this.password = password;
+    this.fullname = fullname;
+    this.role = role ? role : "user";
   }
 
   _verifyPayload({ username, password, fullname, role }: IRegisterUser): void {
     if (username == null || password == null || fullname == null) {
-      throw new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY')
+      throw new Error("REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY");
     }
 
     if (
-      typeof username !== 'string' ||
-      typeof password !== 'string' ||
-      typeof fullname !== 'string' ||
-      typeof role !== 'string'
+      typeof username !== "string" ||
+      typeof password !== "string" ||
+      typeof fullname !== "string"
     ) {
-      throw new Error('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION')
+      throw new Error("REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION");
     }
 
-
     if (username.length > 50) {
-      throw new Error('REGISTER_USER.USERNAME_LIMIT_CHAR')
+      throw new Error("REGISTER_USER.USERNAME_LIMIT_CHAR");
     }
 
     if (username.match(/^[\w]+$/) == null) {
-      throw new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER')
+      throw new Error("REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER");
     }
   }
 }
