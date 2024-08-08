0a30c8fc2ca2f08b75f6a7e1f25f395a96acfa91
AR-Project <alvenresta@gmail.com>
2024-08-08 16:24:47 +0700

    secure store path

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,3 +1,19 @@
+import getAppSession from "@/presentation/utils/getAppSession";
+import { redirect } from "next/navigation";
+
 export default async function Page() {
+  const session = await getAppSession();
+  if (!session) {
+    redirect("/login");
+  }
+
+  const activeStore = session.user.activeStore;
+
+  if (!activeStore) {
+    redirect("/profile");
+  }
+
+  redirect(`./${activeStore}`);
+
   return null;
 }
