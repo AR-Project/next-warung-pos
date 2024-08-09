3d1b35bd4ab335e5eab690e61bff76dce6f64876
AR-Project <alvenresta@gmail.com>
2024-08-09 10:28:59 +0700

    added router

--- src/app/store/[storeId]/manage/menu/new-item/FormAddItem.tsx
+++ src/app/store/[storeId]/manage/menu/new-item/FormAddItem.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useFormState } from "react-dom";
-import { addItem } from "./action";
 import { useEffect } from "react";
-import { toast, ToastContainer } from "react-toastify";
+import { useFormState } from "react-dom";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+import { addItem } from "./action";
 import FormSubmitButton from "@/presentation/component/Form/FormSubmitButton";
 
 type Props = {
@@ -12,10 +14,12 @@ type Props = {
 
 export default function FormAddItem({ categories }: Props) {
   const [state, formAction] = useFormState<FormState, FormData>(addItem, {});
+  const router = useRouter();
 
   useEffect(() => {
     if (state.message) {
       toast.success(state.message);
+      router.back();
     }
     if (state.error) {
       toast.error(state.error);
@@ -23,52 +27,41 @@ export default function FormAddItem({ categories }: Props) {
   }, [state]);
 
   return (
-    <>
-      <form action={formAction} className="flex flex-col gap-1 ">
-        <input type="hidden" name="image-url" value="TODO" />
-        <label htmlFor="item-category">Choose category:</label>
-        <select
-          className="text-black"
-          name="category"
-          id="item-category"
-          required
-        >
-          {categories.map((category) => (
-            <option
-              className="text-black"
-              value={category.id}
-              key={category.id}
-            >
-              {category.name}
-            </option>
-          ))}
-        </select>
-        <label htmlFor="add-item-name">Name: </label>
-        <input
-          className="text-black"
-          type="text"
-          name="name"
-          id="item-name"
-          required
-        />
+    <form action={formAction} className="flex flex-col gap-1 ">
+      <input type="hidden" name="image-url" value="TODO" />
+      <label htmlFor="item-category">Choose category:</label>
+      <select
+        className="text-black"
+        name="category"
+        id="item-category"
+        required
+      >
+        {categories.map((category) => (
+          <option className="text-black" value={category.id} key={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="add-item-name">Name: </label>
+      <input
+        className="text-black"
+        type="text"
+        name="name"
+        id="item-name"
+        required
+      />
 
-        <label htmlFor="item-price">Price: </label>
-        <input
-          type="number"
-          name="price"
-          id="item-price"
-          className="text-black"
-        />
-        <label htmlFor="item-color">Color Label:</label>
-        <input
-          type="color"
-          name="color"
-          id="item-color"
-          defaultValue="#2563eb"
-        />
+      <label htmlFor="item-price">Price: </label>
+      <input
+        type="number"
+        name="price"
+        id="item-price"
+        className="text-black"
+      />
+      <label htmlFor="item-color">Color Label:</label>
+      <input type="color" name="color" id="item-color" defaultValue="#2563eb" />
 
-        <FormSubmitButton>Tambah Item</FormSubmitButton>
-      </form>
-    </>
+      <FormSubmitButton>Tambah Item</FormSubmitButton>
+    </form>
   );
 }
