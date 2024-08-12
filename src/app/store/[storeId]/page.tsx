05a0a0015191e76705b7be4efee3d58d1874a0a5
AR-Project <alvenresta@gmail.com>
2024-08-12 14:29:07 +0700

    refactor links

--- src/app/store/[storeId]/page.tsx
+++ src/app/store/[storeId]/page.tsx
@@ -1,4 +1,5 @@
 import { SlEqualizer } from "react-icons/sl";
+import { MdOutlinePointOfSale } from "react-icons/md";
 import Link from "next/link";
 
 export default async function Page({
@@ -6,16 +7,32 @@ export default async function Page({
 }: {
   params: { storeId: string };
 }) {
+  const links = [
+    {
+      icon: <SlEqualizer className="text-xl" />,
+      label: "Manage",
+      href: "./manage",
+    },
+    {
+      icon: <MdOutlinePointOfSale className="text-xl" />,
+      label: "Sale",
+      href: "#",
+    },
+  ];
   return (
     <div className="flex flex-col gap-2 p-2 mt-5">
-      <p>
-        <Link
-          className="flex flex-row gap-1 fborder rounded-sm text-md w-fit uppercase font-bold items-center bg-blue-600 py-2 px-5 border border-white shadow-sm"
-          href={"./manage"}
-        >
-          <SlEqualizer /> Manage Store
-        </Link>
-      </p>
+      <div className="grid grid-cols-4 gap-1 items-stretch">
+        {links.map((link) => (
+          <Link
+            className="flex flex-col gap-1 rounded-sm text-xs uppercase font-bold items-center bg-blue-600 px-2 py-1 shadow-sm"
+            href={link.href}
+            key={link.href}
+          >
+            {link.icon}
+            {link.label}
+          </Link>
+        ))}
+      </div>
       <p className="text-xs text-gray-500 italic">
         TODO: Display store dynamic information: Total transaction, total gross,
         item sold, etc
